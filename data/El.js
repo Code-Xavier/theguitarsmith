cc27e54f55f18ee36a5909a3beff0dde317cab19
John Doe <johndoe@email.com>
2022-12-13 13:04:34 -0500

    message

--- data/El.js
+++ data/El.js
@@ -2,13 +2,13 @@ const El = [
   {
     title: 'Lesson 1: Monkey With A Guitar ',
     description: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`,
-    imgSrc: '/static/images/shadows.png',
+    imgSrc: '/static/images/monkey-music.jpg',
     href: '/blog/sotc/sotc',
   },
   {
-    title: 'Lesson 2: Beyond Basics ',
+    title: 'Lesson 2: Getting Comfortable ',
     description: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`,
-    imgSrc: '/static/images/throne.png',
+    imgSrc: '/static/images/beyond-basics.jpg',
     href: '/blog/tod/tod',
   },
  
