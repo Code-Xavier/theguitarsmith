cdf2ab61472d63355311d40680d0740d6ac38431
John Doe <johndoe@email.com>
2022-12-19 15:11:13 -0500

    "fixed links"

--- data/El.js
+++ data/El.js
@@ -1,17 +1,16 @@
 const El = [
   {
-    title: 'Lesson 1: Monkey With A Guitar ',
+    title: "Lesson 1: Monkey With A Guitar",
     description: `In lessons one, we go over the very basics of guitar anatomy, and quickly get you practicing your first 4 chords, and strumming your first strumming patterns. We also go over the musical alphabet, and the basics of rhythm.`,
-    imgSrc: '/static/images/monkey-music.jpg',
-    href: '/blog/sotc/sotc',
+    imgSrc: "/static/images/monkey-music.jpg",
+    href: "/blog/english1",
   },
   {
-    title: 'Lesson 2: Getting Comfortable ',
+    title: "Lesson 2: Getting Comfortable",
     description: `In lesson two, we go deeper into the concepts we covered in lesson one, and start introducing some new chords, strumming patterns, and get you started with fingerstyle guitar. This will help you leaarn to play your favorite songs as quickly as possible!`,
-    imgSrc: '/static/images/beyond-basics.jpg',
-    href: '/blog/tod/tod',
+    imgSrc: "/static/images/beyond-basics.jpg",
+    href: "/blog/english2",
   },
- 
-]
+];
 
-export default El
+export default El;
--- data/Sl.js
+++ data/Sl.js
@@ -1,18 +1,16 @@
 const Sl = [
-    {
-      title: 'Lección 1: Mono Con Guitarra ',
-      description: `En la primera lección, repasamos los fundamentos básicos de la anatomía de la guitarra, practicamos rápidamente los 4 primeros acordes y rasgueamos los primeros patrones de rasgueo. También repasamos el alfabeto musical y los fundamentos del ritmo.`,
-      imgSrc: '/static/images/monkey-music.jpg',
-      href: '/blog/sotc/sotc',
-    },
-    {
-      title: 'Lección 2: Más Allá De Los Fundamentos',
-      description: `En la segunda lección, profundizamos en los conceptos tratados en la primera y introducimos algunos acordes nuevos, patrones de rasgueo y a iniciarte en la guitarra fingerstyle. Esto te ayudará a aprender a tocar tus canciones favoritas lo más rápido posible.`,
-      imgSrc: '/static/images/beyond-basics.jpg',
-      href: '/blog/tod/tod',
-    },
-   
-  ]
-  
-  export default Sl
-  
+  {
+    title: "Lección 1: Mono Con Guitarra ",
+    description: `En la primera lección, repasamos los fundamentos básicos de la anatomía de la guitarra, practicamos rápidamente los 4 primeros acordes y rasgueamos los primeros patrones de rasgueo. También repasamos el alfabeto musical y los fundamentos del ritmo.`,
+    imgSrc: "/static/images/monkey-music.jpg",
+    href: "/blog/sl1",
+  },
+  {
+    title: "Lección 2: Más Allá De Los Fundamentos",
+    description: `En la segunda lección, profundizamos en los conceptos tratados en la primera y introducimos algunos acordes nuevos, patrones de rasgueo y a iniciarte en la guitarra fingerstyle. Esto te ayudará a aprender a tocar tus canciones favoritas lo más rápido posible.`,
+    imgSrc: "/static/images/beyond-basics.jpg",
+    href: "/blog/sl2",
+  },
+];
+
+export default Sl;
